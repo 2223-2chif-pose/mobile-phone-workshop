01a1aeb331a34b626ecd5da4034d5a53c430f9b1
Markus Haslinger <markus@marce.at>
2022-11-03 18:32:02 +0100

    state after lesson 2022-11-03

---
+++ MobilePhones.Test/MobilePhoneTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MobilePhones.Test;
+
+public sealed class MobilePhoneTests
+{
+    [Theory]
+    [InlineData("+43664123456")]
+    [InlineData("+43699654321")]
+    [InlineData("0043664123456")]
+    [InlineData("0043650123456789")]
+    public void Construction_NumberOnly_Valid(string phoneNumber)
+    {
+        var mobile = new MobilePhone(phoneNumber);
+
+        mobile.PhoneNumber.Should().Be(phoneNumber);
+        mobile.ValidNumber.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("43664123456", "missing either + or 00 prefix")]
+    [InlineData("004366412345", "too short")]
+    [InlineData("0043764123456", "operator code has to start with 6")]
+    [InlineData("043664123456", "prefix has only one 0")]
+    [InlineData("0043688123456", "unknown operator code")]
+    [InlineData("+0043650123456", "both + and 00 prefix")]
+    [InlineData("+49650123456", "wrong country code")]
+    [InlineData("+4965012E456", "contains a non-digit character")]
+    [InlineData("", "is empty")]
+    public void Construction_NumberOnly_Invalid(string phoneNumber, string reason)
+    {
+        var mobile = new MobilePhone(phoneNumber);
+
+        mobile.PhoneNumber.Should().Be(phoneNumber);
+        mobile.ValidNumber.Should().BeFalse(reason);
+    }
+}
---
+++ MobilePhones/MobilePhone.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MobilePhones;
+
+public sealed class MobilePhone
+{
+    public string PhoneNumber { get; }
+    public bool ValidNumber { get; }
+
+    public MobilePhone(string phoneNumber)
+    {
+        PhoneNumber = phoneNumber;
+        ValidNumber = IsValidPhoneNumber(phoneNumber);
+    }
+
+    private static bool IsValidPhoneNumber(string phoneNumber)
+    {
+        if (string.IsNullOrWhiteSpace(phoneNumber))
+        {
+            return false;
+        }
+
+        var regex = new Regex("^(?:\\+|00)43(6\\d{2})\\d{6,}");
+        var match = regex.Match(phoneNumber);
+
+        if (!match.Success
+            || match.Groups.Count != 2
+            || !int.TryParse(match.Groups[1].Value, out var operatorCode))
+        {
+            return false;
+        }
+
+        var allowedOperators = new[] { 664, 650, 699, 681 };
+        foreach (var @operator in allowedOperators)
+        {
+            if (@operator == operatorCode)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
